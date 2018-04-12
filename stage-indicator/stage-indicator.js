932320314044139597b6603483447babbbf5f96f
Arthur Alexander <arthur.alexander22@gmail.com>
2018-04-12 23:16:53 +0200

    component layout tweaks

--- stage-indicator/stage-indicator.js
+++ stage-indicator/stage-indicator.js
@@ -1,34 +1,66 @@
-(function() {
+(function () {
 
     const owner = document.currentScript.ownerDocument;
 
     class StageIndicator extends HTMLElement {
 
+        static get observedAttributes() {
+            return ["type", "value"];
+        };
+
         constructor() {
             super();
 
-            const shadowRoot = this.attachShadow({ mode: 'open'});
+            const shadowRoot = this.attachShadow({
+                mode: 'open'
+            });
             const template = owner.querySelector('#stage-indicator-template');
             const instance = template.content.cloneNode(true);
 
             shadowRoot.appendChild(instance);
         }
 
-        static get observeredAttributes() {
-            return ["type", "value"];
+        get type() {
+            return this.getAttribute("type");
+        };
+        set type(value) {
+            this.setAttribute("type", value);
+        };
+
+        get value() {
+            return this.getAttribute("value");
+        };
+        set value(value) {
+            this.setAttribute("value", value);
+        };
+
+        connectedCallback() {}
+
+        disconnectedCallback() {}
+
+        attributeChangedCallback(name, oldValue, newValue) {
+
+            switch (name) {
+
+                case 'type':
+                    return this.setStageType(newValue);
+                case 'value':
+                    return this.setStagePosition(newValue);
+            }
         }
 
-        get type() { return this.getAttribute("type"); };
-        set type(value) { this.setAttribute("type", value); };
+        setStageType(type) {
+            let element = this.shadowRoot.querySelector('.stage-indicator');
+            let types = ["major", "minor"];
 
-        get value() { return this.getAttribute("value"); };
-        set value(value) { this.setAttribute("value", value); };
-
-        connectedCallback() {
+            element.classList.remove(...types);
+            element.classList.add(type);
         }
 
-        disconnectedCallback() {
+        setStagePosition() {
+
         }
+
     }
 
     customElements.define('stage-indicator', StageIndicator);
