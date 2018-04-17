f44b33879eb3533a055d3f8bccaca02391fdab58
Arthur Alexander <arthur.alexander22@gmail.com>
2018-04-17 11:12:15 +0200

    Completed stage-indicator progress fill

--- staged-progress/staged-progress.js
+++ staged-progress/staged-progress.js
@@ -3,25 +3,33 @@
     // 1. ES6 modules
 
     const owner = document.currentScript.ownerDocument; // 
-    
+
     class StagedProgress extends HTMLElement {
 
-        
+
         constructor() {
 
             super();
 
-            const shadowRoot = this.attachShadow({ mode: 'open' });
+            const shadowRoot = this.attachShadow({
+                mode: 'open'
+            });
             const template = owner.querySelector('#staged-progress-template'); // move to connected callback
             const instance = template.content.cloneNode(true);
 
             shadowRoot.appendChild(instance);
         }
 
-        static get observedAttributes() { return ["value"]; };
+        static get observedAttributes() {
+            return ["value"];
+        };
 
-        get value() { return this.getAttribute('value'); };
-        set value(value) { this.setAttribute('value', value); };
+        get value() {
+            return this.getAttribute('value');
+        };
+        set value(value) {
+            this.setAttribute('value', value);
+        };
 
         connectedCallback() {
 
@@ -41,7 +49,7 @@
             const element = this.shadowRoot.querySelector('.progress');
             const span = this.shadowRoot.querySelector('.progress > span');
 
-            element.setAttribute('data-progress', value);            
+            element.setAttribute('data-progress', value);
             span.style.width = value + "%"; // css variable for this, width is expansive (transform is less expensive)
         }
     }
